1cdadb96c36761c3f7fac68a1f89a778e558ea28
Devan-R-Mapp <devan.r.mapp@gmail.com>
2024-07-17 22:26:50 -0600

    Mods and spawners and axe change.

---
+++ Scripts/MobPooling.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+var mobScene: PackedScene = preload("res://Scenes/Melee_Shadow.tscn")
+var poolSize: int = 15
+var mob_pool: Array = []
+@onready var timer: Timer = $Timer
+
+
+func _ready() -> void:
+	for i in range(poolSize):
+		var mobTemp: Node = mobScene.instantiate()
+		mobTemp.hide()
+		mob_pool.append(mobTemp)
+		add_child(mobTemp)
+
+func get_mob()-> Node:
+	for mob in mob_pool:
+		if not mob.visible :
+			return mob
+	var new_mob:Node = mobScene.instantiate()
+	new_mob.hide()
+	mob_pool.append(new_mob)
+	add_child(new_mob)
+	return new_mob
+	
+func reset_mob(mob: Node) -> void:
+	mob.position = Vector2(-1000, -1000)
+	mob.get_node("CollisionShape2D").set_deferred("disabled", true)
+	mob.isAlive = false
+	mob.hide()
+
+
+func _on_timer_timeout() -> void:
+	var mobTemp: Node = get_mob()
+	var randX = randi_range(-50,50)
+	var randY = randi_range(-50,50)
+	mobTemp.global_position = self.global_position + Vector2(randX,randY)
+	mobTemp.show()
+
--- Scripts/Player/projectile.gd
+++ Scripts/Player/projectile.gd
@@ -8,6 +8,13 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	self.rotation += 1
+	self.rotation += .20
 	move_and_slide()
 
+
+func _on_damage_box_body_entered(body):
+	if body.get("isMob"):
+		if body.isAlive and body.visible and self.visible:
+			body.reset_mob(body)
+			get_parent().reset_projectile(self)
+		
