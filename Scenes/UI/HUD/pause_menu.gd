1e64a6600aaae512ed71368da560c0059075c7a6
Ifritborn <ifritborndesigns@gmail.com>
2024-07-29 16:18:25 -0600

    timer hud

--- Scenes/UI/HUD/pause_menu.gd
+++ Scenes/UI/HUD/pause_menu.gd
@@ -25,4 +25,7 @@ func _on_resume_pressed():
 
 
 func _on_quit_pressed():
-	get_tree().quit()
+	PM.visible = false
+	get_tree().paused = false
+	paused = false
+	get_tree().change_scene_to_file("res://Scenes/Menus/main_menu.tscn")
