e65ff6cc61633d10eed5c09c5cd1195fdddee770
Ifritborn <ifritborndesigns@gmail.com>
2024-07-30 22:01:52 -0600

    costs display implemented

--- Scripts/UI/HUD_resources.gd
+++ Scripts/UI/HUD_resources.gd
@@ -4,6 +4,9 @@ class_name HUD_resources
 @onready var ore_label = %Ore
 @onready var mercury_label = %Mercury
 
+@export var inventory_item: InventoryItem
+
+
 var ore_quantity = 0:
 	set(new_quantity):
 		ore_quantity = new_quantity
@@ -25,7 +28,7 @@ func _ready():
 	_update_ore_label()
 	_update_mercury_label()
 
-
+	
 func _update_ore_label():
 	ore_label.text = str(ore_quantity)
 	
